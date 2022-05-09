1ce5e4ca9434a482ea8b0dfcffa6a38570685a53
Minh Lys <lyminh1994@gmail.com>
2022-05-09 23:58:45 +0700

    Update JwtService

--- src/main/java/com/minhlq/blogsservice/properties/JwtProperties.java
+++ src/main/java/com/minhlq/blogsservice/properties/JwtProperties.java
@@ -2,8 +2,10 @@ package com.minhlq.blogsservice.properties;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 
 @Data
+@Component
 @ConfigurationProperties(prefix = "jwt")
 public class JwtProperties {
 
--- src/main/java/com/minhlq/blogsservice/service/impl/JwtServiceImpl.java
+++ src/main/java/com/minhlq/blogsservice/service/impl/JwtServiceImpl.java
@@ -20,13 +20,11 @@ import javax.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.apache.commons.lang3.StringUtils;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 @Log4j2
-@Component
+@Service
 @RequiredArgsConstructor
-@EnableConfigurationProperties(JwtProperties.class)
 public class JwtServiceImpl implements JwtService {
 
   private final JwtProperties jwtProperties;
