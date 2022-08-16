5cd0a11309a25663f057afa19c8d95e353437e72
Minh Lys <lyminh1994@gmail.com>
2022-08-16 22:05:02 +0700

    Add ErrorResourceSerializer

--- src/main/java/com/minhlq/blogsservice/exception/handler/ErrorResource.java
+++ src/main/java/com/minhlq/blogsservice/exception/handler/ErrorResource.java
@@ -1,6 +1,7 @@
 package com.minhlq.blogsservice.exception.handler;
 
 import com.fasterxml.jackson.annotation.JsonRootName;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -8,6 +9,7 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 @JsonRootName("errors")
+@JsonSerialize(using = ErrorResourceSerializer.class)
 public class ErrorResource {
 
   private final List<FieldErrorResource> fieldErrors;
---
+++ src/main/java/com/minhlq/blogsservice/exception/handler/ErrorResourceSerializer.java
@@ -0,0 +1,44 @@
+package com.minhlq.blogsservice.exception.handler;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+public class ErrorResourceSerializer extends JsonSerializer<ErrorResource> {
+
+  @Override
+  public void serialize(ErrorResource value, JsonGenerator gen, SerializerProvider serializers)
+      throws IOException {
+    Map<String, List<String>> json = new HashMap<>();
+    gen.writeStartObject();
+    gen.writeObjectFieldStart("errors");
+    for (FieldErrorResource fieldErrorResource : value.getFieldErrors()) {
+      if (!json.containsKey(fieldErrorResource.getField())) {
+        json.put(fieldErrorResource.getField(), new ArrayList<>());
+      }
+      json.get(fieldErrorResource.getField()).add(fieldErrorResource.getMessage());
+    }
+    for (Map.Entry<String, List<String>> pair : json.entrySet()) {
+      gen.writeArrayFieldStart(pair.getKey());
+      pair.getValue()
+          .forEach(
+              content -> {
+                try {
+                  gen.writeString(content);
+                } catch (IOException e) {
+                  log.error("Write object to string error", e);
+                }
+              });
+      gen.writeEndArray();
+    }
+    gen.writeEndObject();
+    gen.writeEndObject();
+  }
+}
