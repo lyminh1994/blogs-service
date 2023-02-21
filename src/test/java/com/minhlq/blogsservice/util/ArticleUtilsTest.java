e38ddb1ec2df399d5c7d0f7bbc41166da556f122
Minh Lys <lyminh1994@gmail.com>
2023-02-21 23:38:12 +0700

    add case blank title

--- src/test/java/com/minhlq/blogsservice/util/ArticleUtilsTest.java
+++ src/test/java/com/minhlq/blogsservice/util/ArticleUtilsTest.java
@@ -1,14 +1,22 @@
 package com.minhlq.blogsservice.util;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class ArticleUtilsTest {
+
+  @Test
+  void givenBlankTitle_whenCallingToSlug_thenReturnBlank() {
+    String actual = ArticleUtils.toSlug("");
+
+    assertEquals("", actual);
+  }
 
   @Test
   void givenTitle_whenCallingToSlug_thenReturnSlug() {
     String actual = ArticleUtils.toSlug("Title 1");
 
-    Assertions.assertEquals("title-1", actual);
+    assertEquals("title-1", actual);
   }
 }
