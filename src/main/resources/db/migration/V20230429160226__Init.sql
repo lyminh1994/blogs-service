6f618036e7ea0ed87710698de4fbc80e30ef1428
Lys Minh <lyminh1994@gmail.com>
2023-04-29 17:03:31 +0700

    add flyway database migration

--- src/main/resources/db/migration/V20230429160226__Init.sql
+++ src/main/resources/db/migration/V20230429160226__Init.sql
@@ -1,19 +1,19 @@
-create sequence if not exists articles_seq increment by 1;
-create sequence if not exists comments_seq increment by 1;
-create sequence if not exists revinfo_seq increment by 50;
-create sequence if not exists roles_seq increment by 1;
-create sequence if not exists tags_seq increment by 1;
-create sequence if not exists users_roles_seq increment by 1;
-create sequence if not exists users_seq increment by 1;
+create sequence articles_seq increment by 1;
+create sequence comments_seq increment by 1;
+create sequence revinfo_seq increment by 50;
+create sequence roles_seq increment by 1;
+create sequence tags_seq increment by 1;
+create sequence users_roles_seq increment by 1;
+create sequence users_seq increment by 1;
 
-create table if not exists roles
+create table roles
 (
     id          bigint not null primary key,
     description varchar(255),
     name        varchar(255)
-);
+    );
 
-create table if not exists users
+create table users
 (
     id                    bigint       not null primary key,
     created_at            timestamp(6),
@@ -35,9 +35,9 @@ create table if not exists users
     profile_image         varchar(255),
     username              varchar(50)  not null unique,
     verification_token    varchar(255)
-);
+    );
 
-create table if not exists users_roles
+create table users_roles
 (
     id         bigint       not null primary key,
     created_at timestamp(6),
@@ -48,16 +48,16 @@ create table if not exists users_roles
     version    smallint     not null,
     role_id    bigint references roles,
     user_id    bigint references users
-);
+    );
 
-create table if not exists follows
+create table follows
 (
     follow_id bigint not null,
     user_id   bigint not null,
     primary key (follow_id, user_id)
-);
+    );
 
-create table if not exists articles
+create table articles
 (
     id          bigint       not null primary key,
     created_at  timestamp(6),
@@ -71,29 +71,29 @@ create table if not exists articles
     slug        varchar(255) unique,
     title       varchar(255),
     user_id     bigint references users
-);
+    );
 
-create table if not exists articles_favorites
+create table articles_favorites
 (
     article_id bigint not null,
     user_id    bigint not null,
     primary key (article_id, user_id)
-);
+    );
 
-create table if not exists tags
+create table tags
 (
     id   bigint       not null primary key,
     name varchar(255) not null
-);
+    );
 
-create table if not exists articles_tags
+create table articles_tags
 (
     article_id bigint not null,
     tag_id     bigint not null,
     primary key (article_id, tag_id)
-);
+    );
 
-create table if not exists comments
+create table comments
 (
     id         bigint       not null primary key,
     created_at timestamp(6),
@@ -105,15 +105,15 @@ create table if not exists comments
     body       varchar(255),
     article_id bigint references articles,
     user_id    bigint references users
-);
+    );
 
-create table if not exists revinfo
+create table revinfo
 (
     rev      integer not null primary key,
     revtstmp bigint
 );
 
-create table if not exists users_aud
+create table users_aud
 (
     id            bigint  not null,
     rev           integer not null references revinfo,
@@ -129,4 +129,4 @@ create table if not exists users_aud
     profile_image varchar(255),
     username      varchar(255),
     primary key (rev, id)
-);
+    );
